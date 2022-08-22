803c0833106736386e04dc1882f385e432b18bc7
Wellington Lima <0wellingtonlima1@gmail.com>
2022-08-22 18:06:21 +0100

    javascript adicionado para resolver alguns pequenos problemas. Passo seguinte: adicionar uma solução para a galeria de imagens na aba bicicletas/nimbus

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Sobre
-## BykCraft
-<p> O site trata-se de uma página fictícia elaborada com o curso Origamid.</p>
+
+## bikcraft
+
+<p> O site trata-se de uma página fictícia elaborada com o curso Origamid.
+<br>
+As Tecnologias usadas, majoritariamente, foram HTML e CSS. JavaScript foi usado para resolver apenas alguns pequenos problemas.</p>
 <br>
 
 ## <p> ⬇️⬇️ Link da página ⬇️⬇️</p>
-<p>❌❌ <a>https://zwellingtonlima.github.io/Bykcraft-portfolio/</a> </p>
+
+<p>❌❌ <a>https://zwellingtonlima.github.io/bikcraft-portfolio/</a> </p>
---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Ativar itens do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link){
+  const url = location.href;
+  const href = link.href;
+
+  if(url.includes(href)){
+    link.classList.add('ativo')
+  }
+}
+links.forEach(ativarLink);
+
+
+// Ativar itens do orçamento. Essa função transfere informações de uma página pra outra através do URl.
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute('aria-expanded', ativa);
+  
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+//Trocar as bikes na galeria na hora da compra
